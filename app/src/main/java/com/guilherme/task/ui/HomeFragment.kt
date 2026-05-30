c87603d87d64bfe9304f0b76a92c3d258da83fcd
¨Guilherme¨ <guilhermecosta030409@gmail.com>
2026-05-30 09:34:56 -0300

    finalizado

---
+++ app/src/main/java/com/guilherme/task/ui/HomeFragment.kt
@@ -0,0 +1,52 @@
+package com.guilherme.task.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.google.android.material.tabs.TabLayoutMediator
+import com.guilherme.task.R
+import com.guilherme.task.databinding.FragmentHomeBinding
+import com.guilherme.task.ui.adapter.ViewPagerAdapter
+
+
+class HomeFragment : Fragment() {
+
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initTabs()
+    }
+
+    private fun initTabs(){
+        val pagerAdapter = ViewPagerAdapter(requireActivity())
+        binding.viewPager.adapter = pagerAdapter
+        pagerAdapter.addFragment(TodoFragment(), R.string.status_task_todo)
+        pagerAdapter.addFragment(TodoFragment(), R.string.status_task_doing)
+        pagerAdapter.addFragment(TodoFragment(), R.string.status_task_done)
+
+        binding.viewPager.offscreenPageLimit = pagerAdapter.itemCount
+
+        TabLayoutMediator(binding.tabs, binding.viewPager){ tab, position ->
+            tab.text = getString(pagerAdapter.getTitle(position))
+        }.attach()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
---
+++ app/src/main/java/com/guilherme/task/ui/auth/LoginFragment.kt
@@ -0,0 +1,52 @@
+package com.guilherme.task.ui.auth
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.guilherme.task.R
+import com.guilherme.task.databinding.FragmentLoginBinding
+
+
+class LoginFragment : Fragment() {
+
+    private var _binding: FragmentLoginBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentLoginBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initListener()
+    }
+
+    private fun initListener(){
+        binding.btnLogin.setOnClickListener {
+            findNavController().navigate(R.id.action_global_homeFragment)
+        }
+
+        binding.btnRegister.setOnClickListener {
+            findNavController().navigate(R.id.action_loginFragment2_to_registerFragment2)
+        }
+
+        binding.btnRecover.setOnClickListener {
+            findNavController().navigate(R.id.action_loginFragment2_to_recoverAccountFragment3)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+}
